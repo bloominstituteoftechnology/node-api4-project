cea49ba8b7770a891bf51194d038275836e4319b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-03 22:10:30 -0500

    use cors middleware
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const server = express();
 const helmet = require('helmet');
 const todosRouter = require('./todos/todosRouter');
+const cors = require("cors")
 
 //middlware, if any
+server.use(cors())
 server.use(helmet());
 server.use(express.json());
 server.use('/api/todos', todosRouter);
