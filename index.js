472f4ee702d21c2275f7be45f3cd09d71f9a0fe2
Eric Wood <epwood1@gmail.com>
2020-03-19 18:45:30 -0700

    Fixed scripts and port number

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // code away!
 const server = require('./server.js');
-const port_number = 4000;
+const port_number = process.env.PORT || 4000;
 
 server.listen(port_number, () => console.log(`Listening on port ${port_number}`))
