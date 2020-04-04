4226794f22eee62c5583f813ddc5dad0de717718
wais <waisalmakaleh@gmail.com>
2020-04-04 15:10:05 -0400

    fixed

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const server = require('./server');
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http:localhost:${port} ***\n`);
+});
