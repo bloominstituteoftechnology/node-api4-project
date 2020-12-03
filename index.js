50ce952f378a8f7a926164a96afc03d86dc7d3d9
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-03 17:56:41 -0500

    second commit

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const path = require('path')
 
 const port = process.env.PORT || 5000
 
+console.log('hi my name is paul')
+console.log(__dirname)
+console.log(process.env.PAUL)
+console.log(process.env.PORT)
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 // api
 app.use('/api/*',  (_,  res) => {
