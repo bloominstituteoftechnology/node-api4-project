b78024d974928a933b08ed0246dbe3c0e981d6fd
Rigo <limon1285@gmail.com>
2021-01-21 23:48:06 -0600

    node-env

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require("knex");
 const config = require("../knexfile");
 const environment = process.env.NODE_ENV || "development";
-module.exports = knex(config.development);
+module.exports = knex(config[environment]);
