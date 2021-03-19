320fb5bb34b5c9e0e543571ce47e5834ace495bd
kato hawkins <kristhahawkins@icloud.com>
2021-03-18 23:26:19 -0400

    first real commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/*",(_,res) =>{
+    res.json({data: "The api lives!!"})
+})
+
+app.listen(port, () =>{
+    console.log(`server is alive on port ${port}`)
+})
+
+
+
+console.log("It's alive")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USER)
+console.log(process.env.PORT)
+console.log(dotenv)
