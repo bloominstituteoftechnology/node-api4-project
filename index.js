6fc455dae2d76c5205c4ecd2e397d8f8ced4ca7e
alextran0899 <alextran@MacBook-Air.local>
2021-05-06 20:53:49 -0500

    update

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-console.log('it is working')
 const express = require('express')
-
+const path = require('path')
 const server = express()
 
-const path = require('path')
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 9000
 
 server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.use(express.json())
 
-server.get('/api/*', (req, res)=>{
-    res.json({
-        cohort:'web 41'
-    })
+server.get('/api/*', (req,res)=>{
+    res.json({message: 'api is working'})
 })
 
-server.use('*', (req,res) =>{
+server.use('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
+
+// server.use((err, req, res, next) =>{
+//     res.status(500).json({
+//         message:err.message,
+//         stack: err.stack
+//     })
+// })
 
 server.listen(port, ()=>{
     console.log(`listening on ${port}`)
