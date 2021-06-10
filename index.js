dae16201680506bc6a4cef39907e8ec607c6cef0
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-09 18:54:44 -0700

    Project Complete

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const dotenv = require("dotenv").config()
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
-
-
 const port = process.env.PORT || 5000
+const path= require("path");
 
 app.use(cors());
 app.use(express.json());
+// go find static assets and run them on the server
+app.use(express.static(path.join(__dirname,"client/build")));
 
-app.use("/api/", (_,res)=>{
+app.use("/api/",(_,res)=>{
     res.json({data: "The API is serving data! Can put anything here!"})
 })
 
 app.listen(port,() => {
-    console.log(`Server runnin gon port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
 
 
