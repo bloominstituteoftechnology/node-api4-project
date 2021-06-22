8216e6aa4827ab583c4281990a0e200ab5317b82
Rigo Limon <limon1285@gmail.com>
2021-06-22 16:53:02 -0500

    updated comments section and added middleware to it

--- comments/comments-router.js
+++ comments/comments-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { resource } = require("../api/server");
 const router = express.Router();
 const Comments = require("./comments-model");
+const { checkCommentID } = require("../comments/comments-middleware");
 
 //GET /api/comments
 router.get("/", (req, res, next) => {
@@ -15,15 +15,8 @@ router.get("/", (req, res, next) => {
 });
 
 //GET /api/comments/:id
-router.get("/:id", (req, res, next) => {
-  const { id } = req.params;
-  Comments.getById(id)
-    .then((comment) => {
-      comment
-        ? res.status(200).json(comment)
-        : res.status(404).json({ error: "cant find comment" });
-    })
-    .catch((err) => next(err));
+router.get("/:id", checkCommentID(), (req, res, next) => {
+  res.status(200).json(req.commentID);
 });
 
 //POST /api/comments
@@ -34,6 +27,32 @@ router.post("/", (req, res, next) => {
       comment
         ? res.status(201).json(comment)
         : res.status(404).json({ error: `can't post that user of ${body}` });
+    })
+    .catch((err) => next(err));
+});
+
+//DELETE /api/comments/:id
+router.delete("/:id", checkCommentID(), (req, res, next) => {
+  const { id } = req.params;
+  Comments.deleteComment(id)
+    .then((post) => {
+      console.log("post---->", post);
+      post
+        ? res.status(200).json({ deleted: post })
+        : res.status(404).json({ error: `can't delete user by ${id}` });
+    })
+    .catch((err) => next(err));
+});
+
+//UPDATE /api/comments/:id
+router.put("/:id", (req, res, next) => {
+  const commentBody = req.body;
+  const { id } = req.params;
+
+  Comments.updateComment(id, commentBody)
+    .then((comment) => {
+      res.status(200).json(comment);
+      console.log("comment update--->", comment);
     })
     .catch((err) => next(err));
 });
