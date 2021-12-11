9e2b22395062b30e57270b2dba0746178866b6fb
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-10 21:21:32 -0600

    done with setting up index

---
+++ index.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 9000
+
+
+// console.log("Server is live")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USER)
+// console.log(process.env.APIKEY)
+// console.log(process.env.PORT)
+// console.log(process.env.DBPASS)
