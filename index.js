b073312a5d40cd80154067d3553c0c6d06e49595
Anthony Climer <anthonyclimer0925@gmail.com>
2023-04-25 19:51:40 -0400

    I have completed the project

--- index.js
+++ index.js
@@ -9,10 +9,40 @@ const port = process.env.PORT || 9000 //eslint-disable-line
 server.use(express.json())
 server.use(cors())
 
-server.get('/api/hello', (req,res ) => {
+server.get('/api/users', (req,res ) => {
     res.json({
-        message: 'API id working'
+        message: 'API working',
+        array: ['John', 'Ben', "codsworth", 'McCreedy']
     })
+})
+
+server.post('/api/register', (req,res) => {
+    const { username, password } = req.body
+    const database = [username,password]
+    if(!username || !password) {
+        res.status(401).json({
+            message:'Please complete the form'
+        })   
+    } else {
+        res.status(201).json({
+            message:'Thank you for registering',
+            data: database
+        })
+    }
+})
+
+server.post('/api/login', (req,res) => {
+    const { username, password } = req.body
+    const database = [{username:username }, {password:password}]
+    if(database.username !== username || database.password !== password) {
+        res.status(401).json({
+            message:'Wrong password or email'
+        })   
+    } else {
+        res.status(201).json({
+            message:'Welcome back :)'
+        })
+    }
 })
 
 server.use('*', (req, res ) => {
